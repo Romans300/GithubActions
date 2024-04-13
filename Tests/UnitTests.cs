47b466d7bcb85f98a0fc047928e590be0283f66a
Romans300 <111927403+Romans300@users.noreply.github.com>
2024-04-12 21:40:02 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,13 +1,13 @@
-﻿using System;
 using NUnit.Framework;
+using System;
 
-namespace GithubActionsLab
+namespace GithubActionsLab.Tests
 {
     [TestFixture]
-    public class Math
+    public class ProgramTests
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_DashelRomanski()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_Invalid_DashelRomanski()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,13 +23,107 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_Null_DashelRomanski()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Valid_DashelRomanski()
+        {
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(1, Program.Subtract("5", "4"));
+            Assert.AreEqual(-2, Program.Subtract("3", "5"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_DashelRomanski()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_DashelRomanski()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_DashelRomanski()
+        {
+            Assert.AreEqual(6, Program.Multiply("2", "3"));
+            Assert.AreEqual(15, Program.Multiply("5", "3"));
+            Assert.AreEqual(-10, Program.Multiply("-2", "5"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_DashelRomanski()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_DashelRomanski()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_DashelRomanski()
+        {
+            Assert.AreEqual(2, Program.Divide("6", "3"));
+            Assert.AreEqual(5, Program.Divide("15", "3"));
+            Assert.AreEqual(-2, Program.Divide("-10", "5"));
+        }
+
+        [Test]
+        public void Divide_Invalid_DashelRomanski()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_DashelRomanski()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_DashelRomanski()
+        {
+            Assert.AreEqual(8, Program.Power("2", "3"));
+            Assert.AreEqual(27, Program.Power("3", "3"));
+            Assert.AreEqual(16, Program.Power("2", "4"));
+        }
+
+        [Test]
+        public void Power_Invalid_DashelRomanski()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_DashelRomanski()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
